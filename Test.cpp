9a6c826d00ec22766c70a028851c0dcf4a37ba4f
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-06-11 12:13:47 +0300

    100 tests?

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include "doctest.h"
+#include <iostream>
+#include <stdexcept>
+
+#include <cassert>
+
+TEST_CASE("range"){
+    int j = 0;
+	for(int ch=0;ch<101;ch++){
+    for(int i : range(0,20)){
+        CHECK(i == j++);
+	}
+	j=0;i=0;
+    }
+}
