aafdeb1e86d1c1d0387323dd4311e971f0fb8b1c
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-06-15 20:15:47 +0300

    test 100

--- Test.cpp
+++ Test.cpp
@@ -1,46 +1,21 @@
-//
-//  Demo for itertools exercise.
-//
-//  Version 1 by Adam Lev-Ari on 02/2020. Copyright © 2020 Adam Lev-Ari. All rights reserved.
-//  Version 2 by Erel Segal-Halevi on 06/2020.
-//
-
+#include "doctest.h"
 #include <iostream>
 #include <vector>
+#include <limits>
 
 #include "range.hpp"
 #include "accumulate.hpp"
 #include "filterfalse.hpp"
 #include "compress.hpp"
-#include "doctest.h"
 
 
-using namespace std;
 using namespace itertools;
+using namespace std;
 
 
-TEST_CASE("check addFather")
-{
-vector<int> vecint = {5 ,6 ,7, 8};
-	for (int i: itertools::range(5,9)){
-
-    CHECK(i==vecint[i]);
-}
- vecint = {0 ,1 ,2, 3};
-	for (int i: itertools::range(0,4)){
-
-    CHECK(i==vecint[i]);
-}
-vecint = {105 ,106 ,107, 108};
-	for (int i: itertools::range(105,109)){
-
-    CHECK(i==vecint[i]);
-}
-}
-
-TEST_CASE("check addFather"){
-vector<int> vecint = {5 ,11, 18, 26};
-
-for (int i: itertools::accumulate(itertools::range(5,9))){
-    CHECK(i==vecint[i]);}
+TEST_CASE("range"){
+    int j = 0;
+    for (int i: range(0,101)){
+        CHECK(i == j++);
+    }
 }
