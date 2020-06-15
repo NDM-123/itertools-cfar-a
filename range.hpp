cbdf133be8c8b2523feccf36326703ab26c6b9aa
NDM-123 <57434608+NDM-123@users.noreply.github.com>
2020-06-15 20:17:37 +0300

    new range

--- range.hpp
+++ range.hpp
@@ -1,18 +1,29 @@
 
 #pragma once
+
+#include <vector>
+#include <stdexcept>
 namespace itertools{
 
-template <typename T>
-std::vector<T> range(T begin,T end){
-std::vector<T> a;
-return a;
+template <typename IntType>
+std::vector<IntType> range(IntType start, IntType stop)
+{
+IntType step=1;
+  if (step == IntType(0))
+  {
+    throw std::invalid_argument("step for range must be non-zero");
+  }
+
+  std::vector<IntType> result;
+  IntType i = start;
+  while ((step > 0) ? (i < stop) : (i > stop))
+  {
+    result.push_back(i);
+    i += step;
+  }
+
+  return result;
 }
-   ///int_iterator begin(){
-      //      return int_iterator(_begin);
-      //  }
-      //  int_iterator end(){
-       //     return int_iterator(_end);
-      //  }
 
 
 }
